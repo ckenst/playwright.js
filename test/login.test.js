4f41c3063d5f602c972966081c93e263ecdbf973
Chris Kenst <ckenst@gmail.com>
2020-11-15 23:42:02 -0800

    Working on the login test

---
+++ test/login.test.js
@@ -0,0 +1,18 @@
+const { firefox } = require('playwright');
+const assert = require('assert');
+
+(async () => {
+  const browser = await firefox.launch({ headless: false, slowMo: 50 });
+  const page = await browser.newPage();
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.fill('#username', 'tomsmith');
+  await page.fill('#password', 'SuperSecretPassword!');
+  await page.click('button[type="submit"]');
+
+  const successText = await page.innerText('#flash');
+  console.log(successText);
+  assert(successText === 'You logged into a secure area!');
+
+  // await page.screenshot({ path: `example.png` });
+  await browser.close();
+})();
